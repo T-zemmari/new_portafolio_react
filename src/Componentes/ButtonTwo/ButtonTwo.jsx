ac576a89b2520963a0f6b074bebaafe9c9068c5b
Tarek <tarek@tedybusiness.com>
2024-03-18 16:57:19 +0100

    Hero:se ha modificado el btn contactame

---
+++ src/Componentes/ButtonTwo/ButtonTwo.jsx
@@ -0,0 +1,11 @@
+import './ButtonTwo.css';
+
+const ButtonTwo = () => {
+  return (
+    <>
+      <button className='button_two_custom'>Contacta me</button>
+    </>
+  );
+};
+
+export default ButtonTwo;
--- src/Componentes/Hero/Hero.jsx
+++ src/Componentes/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import { my_styles } from "../../my_styles.js";
 import { miImagUno } from "../../constantes.js";
 import ButtonOne from "../ButtonOne/ButtonOne.jsx";
+import ButtonTwo from "../ButtonTwo/ButtonTwo.jsx";
 
 const Hero = () => {
   return (
+    <>
     <section className={`w-full h-full flex flex-col md:flex-row md:items-center mt-28 lg:mt-52 px-10 py-0 lg:px-40 lg:py-5 `}>
       <div className={`w-full h-5/6 sm:w-3/6 lg:w-4/6 ${my_styles.paddingX} flex flex-row items-start justify-center gap-5`}>
         <div className={`flex flex-col justify-start items-center `}>
@@ -21,7 +23,8 @@ const Hero = () => {
             web completas.
           </p>
           <div className="w-full flex flex-row justify-start gap-2 mt-12">
-            <ButtonOne animation={"animate-left-right"}>Contacto</ButtonOne>
+            {/*<ButtonOne animation={"animate-left-right"}>Contacto</ButtonOne>*/}
+            <ButtonTwo/>
           </div>
         </div>
       </div>
@@ -30,6 +33,7 @@ const Hero = () => {
         <div className={`contenedor_imagnen w-60 max-w-96 h-96  mt-20 md:mt-0 border border-indigo-600 rounded-full bg-[url('${miImagUno}')]  bg-center bg-cover`}></div>
       </div>
     </section>
+    </>
   );
 };
 
