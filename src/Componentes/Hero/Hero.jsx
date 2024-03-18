bffdf5c62c5bbac49f1537e58361e4347246ed9b
Tarek <tarek@tedybusiness.com>
2024-03-18 17:17:00 +0100

    Hero:se ha modificado el btn contactame

--- src/Componentes/Hero/Hero.jsx
+++ src/Componentes/Hero/Hero.jsx
@@ -1,13 +1,12 @@
 import { my_styles } from "../../my_styles.js";
 import { miImagUno } from "../../constantes.js";
-import ButtonOne from "../ButtonOne/ButtonOne.jsx";
 import ButtonTwo from "../ButtonTwo/ButtonTwo.jsx";
 
 const Hero = () => {
   return (
     <>
-    <section className={`w-full h-full flex flex-col md:flex-row md:items-center mt-28 lg:mt-52 px-10 py-0 lg:px-40 lg:py-5 `}>
-      <div className={`w-full h-5/6 sm:w-3/6 lg:w-4/6 ${my_styles.paddingX} flex flex-row items-start justify-center gap-5`}>
+    <section className={`w-full h-full flex flex-col md:flex-row md:items-start mt-28 lg:mt-32 px-10 py-0 lg:px-40 lg:py-5 `}>
+      <div className={`w-full h-5/6 sm:w-3/6 lg:w-3/6 ${my_styles.paddingX} flex flex-row items-start justify-center gap-5`}>
         <div className={`flex flex-col justify-start items-center `}>
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 h-40 sm:h-80  bg-[#915EFF]" />
@@ -29,8 +28,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className={`w-full sm:w-3/6 lg:w-2/6 h-5/6 ${my_styles.paddingX} md:-mt-0  flex flex-row justify-center items-start`}>
-        <div className={`contenedor_imagnen w-60 max-w-96 h-96  mt-20 md:mt-0 border border-indigo-600 rounded-full bg-[url('${miImagUno}')]  bg-center bg-cover`}></div>
+      <div className={`w-full sm:w-3/6 lg:w-3/6 h-6/6 ${my_styles.paddingX} md:-mt-0  flex flex-row justify-center items-start`}>
+        <div className={`contenedor_imagnen w-[55rem] max-w-96 h-[50rem] md:mt-0 border border-indigo-600 rounded-full bg-[url('${miImagUno}')]  bg-center bg-cover`} style={{backgroundImage:`url(${miImagUno})`}}></div>
       </div>
     </section>
     </>
