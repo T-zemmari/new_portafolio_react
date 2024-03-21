9c73c39826da2b83c2b2abb3102f581d58863163
Tarek <tarek@tedybusiness.com>
2024-03-21 12:00:55 +0100

    About:maquetando las tarjetas

--- src/Componentes/About/About.jsx
+++ src/Componentes/About/About.jsx
@@ -19,56 +19,78 @@ const About = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const contenido = `
+  const texto_1 = `
     <p class="text-white">
-      Soy un apasionado desarrollador con una amplia experiencia en PHP
-      y otras tecnologías relacionadas. Durante los últimos años, me he
-      destacado como experto en PHP, trabajando en una variedad de
-      proyectos desafiantes que han fortalecido mis habilidades y
-      ampliado mi conocimiento en el campo del desarrollo web.
-    </p>
-    <p class="text-white">
-      Como experto en PHP, he liderado el desarrollo de sistemas web
-      complejos, desde sitios web dinámicos hasta aplicaciones
-      empresariales a gran escala. Mi experiencia abarca desde la
-      creación de aplicaciones desde cero hasta la optimización y el
-      mantenimiento de sistemas existentes. He trabajado con frameworks
-      como Symfony, React, Django para desarrollar aplicaciones web
-      robustas y escalables. Estos frameworks me han permitido acelerar
-      el proceso de desarrollo y mantener un código limpio y modular.
-    </p>
-    <p class="text-white">
-      Además de mi experiencia en PHP, también tengo conocimientos
-      sólidos en tecnologías complementarias como MySQL, para el
-      almacenamiento y la gestión de datos. He diseñado y optimizado
-      bases de datos complejas para garantizar un rendimiento óptimo y
-      una escalabilidad sin problemas.
-    </p>
-    <p class="text-white">
-      Mi enfoque en la seguridad, el rendimiento y la usabilidad me ha
-      permitido crear aplicaciones web que no solo son funcionales y
-      eficientes, sino también fáciles de usar y atractivas para los
-      usuarios finales. En resumen, soy un desarrollador altamente
-      capacitado y motivado que está comprometido a ofrecer soluciones
-      de alta calidad que cumplan con los requisitos del cliente y
-      superen sus expectativas. Estoy emocionado de seguir creciendo y
-      aprendiendo en el emocionante mundo del desarrollo web.
+      Como especialista en frontend, me enfoco en crear interfaces atractivas y funcionales utilizando HTML, CSS, y JavaScript, así como frameworks como React.
+      Desarrollo experiencias dinámicas y receptivas que cautivan a los usuarios y mejoran la interacción con el sitio web.
     </p>
   `;
 
-  const createMarkup = (htmlString) => {
-    return { __html: htmlString };
+  const texto_2 = `
+    <p class="text-white">
+      Lidero el desarrollo de sistemas web complejos, desde sitios dinámicos hasta aplicaciones empresariales. He trabajado con Symfony, React y Django para crear aplicaciones robustas y escalables, acelerando el proceso de desarrollo y manteniendo un código limpio y modula
+    </p>
+  `;
+
+  const texto_3 = `
+    <p class="text-white">
+      Como profesional en bases de datos, diseño y administro sistemas eficientes y escalables. 
+      Utilizo modelado y consultas SQL para asegurar estructuras sólidas y acceso eficiente a la información, 
+      optimizando y asegurando la integridad de los datos.
+    </p>
+  `;
+
+  const addLineBreaks = (text) => {
+    // Añadir un salto de línea después de cada punto
+    return text.replace(/\.\s+/g, ".<br/>");
   };
 
+  const cardsData = [
+    {
+      color: "#262726",
+      //borderColor: "#FF0040", // Rojo eléctrico
+      borderColor: "#ff004061", 
+      content: addLineBreaks(texto_1),
+      title: "FRONTEND",
+    },
+    {
+      color: "#262726",
+      //borderColor: "#00FFFF", // Azul eléctrico
+      borderColor: "#00ffff4f",
+      content: addLineBreaks(texto_2),
+      title: "BACKEND",
+    },
+    {
+      color: "#262726",
+      //borderColor: "#FF00FF", // Violeta eléctrico
+      borderColor: "#ff00ff52", 
+      content: addLineBreaks(texto_3),
+      title: "BASES DE DATOS",
+    },
+  ];
+
   return (
-    <div className="contenedor_about w-full h-[600px] flex flex-col justify-center items-center bg-black p-24">
-      <div className="contenedor_header_about flex justify-center items-center w-full h-[30px] border-b-4  border-orange-700 text-white text-[24px] font-bold py-12">
+    <div className="contenedor_about w-full min-h-[600px] flex flex-col justify-center items-center mt-10 p-12 md:p-24 md:mt-0">
+      <div className="contenedor_header_about flex justify-center items-center w-full h-[30px] py-14 border-b-4  border-orange-700  text-white md:text-[24px] font-bold md:py-12 ">
         {displayText}
       </div>
-      <div
-        className="contenedor_body_about w-5/6 h-[400px] mt-10 border-1 border-b-amber-950"
-        dangerouslySetInnerHTML={createMarkup(contenido)}
-      ></div>
+      <div className="contenedor_body_about w-full grid grid-cols-1 gap-4 mt-10 sm:grid-cols-2 md:grid-cols-3 ">
+        {cardsData.map((card, index) => (
+          <div
+            key={index}
+            className="card rounded-lg overflow-hidden shadow-lg"
+            style={{ backgroundColor: card.color,border: `2px solid ${card.borderColor}` }}
+          >
+            <div className="font-bold text-xl mb-2 text-white flex justify-center items-center h-16">
+              <h3 className="border-b-4 border-orange-700 py-2">{card.title}</h3>
+            </div>
+            <div
+              className="text-gray-200 text-base  px-8 py-4 leading-6 pb-8 font-semibold"
+              dangerouslySetInnerHTML={{ __html: card.content }}
+            ></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
