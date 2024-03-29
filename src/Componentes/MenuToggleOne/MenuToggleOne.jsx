f47803b33ab9cb26f02ec2b26da9a911ec10170f
guirep <guirep@gmail.com>
2024-03-29 22:38:09 +0100

    menutoogle ahora esta abierto y ya tiene la logica de hacer scroll hacia el objetivo

--- src/Componentes/MenuToggleOne/MenuToggleOne.jsx
+++ src/Componentes/MenuToggleOne/MenuToggleOne.jsx
@@ -6,26 +6,43 @@ import {
   icnWorks1,
   bgWave1,
 } from "../../constantes.js";
-import './MenuToggleOne.css';
+import "./MenuToggleOne.css";
 
 const MenuToggleOne = () => {
   const menuRef = useRef(null);
-  const [menuToggled, setMenuToggled] = useState("hidden");
+  const [menuToggled, setMenuToggled] = useState("");
   const [isButtonClicked, setisButtonClicked] = useState(false);
   const handelMenuToggle = () => {
     setMenuToggled(menuToggled === "hidden" ? "" : "hidden");
-    setisButtonClicked(!isButtonClicked)
+    setisButtonClicked(!isButtonClicked);
+  };
+
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const offset = section.offsetTop - 78; // Calcula la posición 50px antes del inicio de la sección
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
   };
   return (
     <>
       <div
         className={`w-full h-[386px] border-0 border-indigo-600 flex flex-column justify-center items-center relative`}
-        style={{ backgroundImage: `url(${bgWave1})`,backgroundRepeat:'no-repeat',backgroundSize:'cover' }}
+        style={{
+          backgroundImage: `url(${bgWave1})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
       >
         <div className="relative">
           <button
             ref={menuRef}
-            className={`w-[100px] h-[100px] rounded-full bg-[#ec4104] text-white ${isButtonClicked ? "border-t-8" : ""} focus:outline-none transition-all duration-200`}
+            className={`w-[100px] h-[100px] rounded-full bg-[#ec4104] text-white ${
+              isButtonClicked ? "border-t-8" : ""
+            } focus:outline-none transition-all duration-200`}
             onClick={handelMenuToggle}
           >
             MENU
@@ -33,18 +50,22 @@ const MenuToggleOne = () => {
           <button
             className={`menu_buttons ${menuToggled}  w-[50px] h-[50px] border-0 rounded-full  absolute -top-0 -left-[60px] bg-center bg-cover hover:w-[60px] hover:h-[60px] hover:-left-[70px]`}
             style={{ backgroundImage: `url(${icnContact1})` }}
+            onClick={()=>scrollToSection('Contacto')}
           ></button>
           <button
             className={`menu_buttons ${menuToggled} w-[50px] h-[50px] border-0 rounded-full  absolute -top-16 -left-4 bg-center bg-cover hover:w-[60px] hover:h-[60px] hover:-left-[20px]`}
             style={{ backgroundImage: `url(${icnExper1})` }}
+            onClick={()=>scrollToSection('Experiencias')}
           ></button>
           <button
             className={`menu_buttons  ${menuToggled} w-[50px] h-[50px] border-0 rounded-full  absolute -top-16 left-16 bg-center bg-cover hover:w-[60px] hover:h-[60px] hover:left[-30px]`}
             style={{ backgroundImage: `url(${icnWorks1})` }}
+            onClick={()=>scrollToSection(`Mis-trabajos`)}
           ></button>
           <button
             className={`menu_buttons  ${menuToggled} w-[50px] h-[50px] border-0 rounded-full  absolute -top-0 left-[110px] bg-center bg-cover hover:w-[60px] hover:h-[60px] hover:left-[118px]`}
             style={{ backgroundImage: `url(${icnTitul1})` }}
+            onClick={()=>scrollToSection('Estudios')}
           ></button>
         </div>
       </div>
