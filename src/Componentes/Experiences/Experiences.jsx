cc2f95e032c5a17007c55998cdbf37e1ff09dc82
guirep <guirep@gmail.com>
2024-04-05 20:12:24 +0200

    Experiencias

--- src/Componentes/Experiences/Experiences.jsx
+++ src/Componentes/Experiences/Experiences.jsx
@@ -12,8 +12,13 @@ const Experiences = () => {
       const experiences = container.querySelectorAll(".experiencia");
       experiences.forEach((experience, index) => {
         const rect = experience.getBoundingClientRect();
-        const distanceFromCenter = Math.abs(rect.top + rect.height / 2 - window.innerHeight / 2);
-        const scale = Math.min(1, Math.max(0, 1 - distanceFromCenter / window.innerHeight));
+        const distanceFromCenter = Math.abs(
+          rect.top + rect.height / 2 - window.innerHeight / 2
+        );
+        const scale = Math.min(
+          1,
+          Math.max(0, 1 - distanceFromCenter / window.innerHeight)
+        );
 
         experience.style.transform = `scale(${scale})`;
       });
@@ -30,19 +35,27 @@ const Experiences = () => {
       ref={containerRef}
     >
       <h1 className="text-[#b7b7b7] text-[20px] font-bold">Experiencias</h1>
-      <div className="flex justify-start items-center w-full h-[30px] text-white md:text-[38px] font-bold md:py-6 md:mt-2" id="contenedor_mi_trayectoria_laboral">
+      <div
+        className="flex justify-start items-center w-full h-[30px] text-white md:text-[38px] font-bold md:py-6 md:mt-2"
+        id="contenedor_mi_trayectoria_laboral"
+      >
         MI TRAYECTORIA LABORAL
       </div>
-      <div className="contenedor_tarjetas_experiencias w-full h-[100%] p-10 mt-24 flex flex-col gap-0">
+      <div className="contenedor_tarjetas_experiencias w-full h-[100%] p-10 mt-24 flex flex-col gap-0 justify-center items-center ">
         {experiencias.map((experience, index) => (
           <div
             key={experience.id}
-            className={`experiencia w-full min-h-[250px] border-2 border-indigo-950 p-6 flex flex-row justify-between
-              ${index % 2 === 0 ? "contenedor_izquierdo" : "contenedor_derecho"} text-white p-4`}
+            className={`experiencia w-[90%] min-h-[250px] border-2 border-indigo-950 p-16 flex flex-col 
+            justify-startr items-center gap-4 bg-[#161455] rounded-xl
+              ${
+                index % 2 === 0 ? "contenedor_izquierdo" : "contenedor_derecho"
+              } text-white p-4`}
           >
-            <div className="titulo">{experience.title}</div>
-            <div className="company_name">{experience.company_name}</div>
-            <div className="date">{experience.date}</div>
+            <div className="titulo h-[40px] experience_title text-4xl font-bold uppercase">
+              {experience.title}
+            </div>
+            <div className="company_name text-2xl">{experience.company_name}</div>
+            <div className="date text-2xl">{experience.date}</div>
             <ul className="points">
               {experience.points.map((point, index) => (
                 <li key={index}>{point}</li>
