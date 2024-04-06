ab6a1b1be6cec2e53798d24c75560024b77d2387
guirep <guirep@gmail.com>
2024-04-06 13:11:26 +0200

    he instalado de momento la libreria emailsjs y he añadido la funcionalidad de envio de emails

--- src/Componentes/Contact/Contact.jsx
+++ src/Componentes/Contact/Contact.jsx
@@ -1,58 +1,70 @@
-import { useState } from 'react';
-import './Contact.css';
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import "./Contact.css";
+import SwAlert from "../SwAlert/SwAlert";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    nombre: '',
-    email: '',
-    mensaje: ''
+    nombre: "",
+    email: "",
+    mensaje: "",
   });
 
   const [errors, setErrors] = useState({
-    nombre: '',
-    email: '',
-    mensaje: ''
+    nombre: "",
+    email: "",
+    mensaje: "",
   });
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
-    setErrors({ ...errors, [id]: '' });
-  };
-
-  const handleSubmit = () => {
-    let valid = true;
-    const newErrors = { ...errors };
-
-    // Validar el nombre
-    if (formData.nombre.trim() === '') {
-      newErrors.nombre = 'Por favor, ingresa tu nombre';
-      valid = false;
-    }
-
-    // Validar el email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(formData.email)) {
-      newErrors.email = 'Por favor, ingresa un correo electrónico válido';
-      valid = false;
-    }
-
-    // Validar el mensaje
-    if (formData.mensaje.trim() === '') {
-      newErrors.mensaje = 'Por favor, ingresa tu mensaje';
-      valid = false;
-    }
-
-    if (valid) {
-      console.log('Formulario enviado:', formData);
+  
+    if (value.trim() === '') {
+      setErrors({ ...errors, [id]: `Por favor, ingresa tu ${id}` });
     } else {
-      setErrors(newErrors);
+      setErrors({ ...errors, [id]: '' });
+    }
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { nombre, email, mensaje } = formData;
+
+    if (nombre !== "" && email !== "" && mensaje !== "") {
+      emailjs
+        .send(
+          "service_vbri4b8",
+          "template_9k463tc",
+          {
+            nombre,
+            email,
+            mensaje,
+          },
+          "J5IKpSLiG5SGLK5pD"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            SwAlert(`Tu mensaje ha sido enviado correctamente Gracias!`);
+          },
+          (error) => {
+            console.log(error.text);
+            SwAlert(`Error!`);
+          }
+        );
+    } else {
+      SwAlert(`Porfavor rellena al formulario correctamente`);
     }
   };
 
   return (
     <>
-      <div className="contenedor_contact flex flex-col mt-16 p-12 md:py-2 md:px-24 md:mt-0" id="Contacto">
+      <div
+        className="contenedor_contact flex flex-col mt-16 p-12 md:py-2 md:px-24 md:mt-0"
+        id="Contacto"
+      >
         <h1 className="text-[#b7b7b7] text-[20px] font-bold">Contacto</h1>
         <div className="flex justify-start items-center w-full h-[30px] text-white md:text-[38px] font-bold md:py-6 md:mt-2">
           RELLENA EL SIGUIENTE FORMULARIO
@@ -64,15 +76,43 @@ const Contact = () => {
             <div className="contacto_campos md:w-[80%] py-12 px-24">
               <h3 className="text-white font-bold">Contacta-me</h3>
               <p>
-                Siéntete libre de contactarnos en cualquier momento. Nos pondremos en contacto contigo tan pronto como sea posible!
+                Siéntete libre de contactarnos en cualquier momento. Nos
+                pondremos en contacto contigo tan pronto como sea posible!
               </p>
-              <input type="text" className="form-control form-group" placeholder="Nombre" id="nombre" onChange={handleChange} />
-              {errors.nombre && <small className="text-[#c53568]">{errors.nombre}</small>}
-              <input type="text" className="form-control form-group" placeholder="Email" id="email" onChange={handleChange} />
-              {errors.email && <small className="text-[#c53568]">{errors.email}</small>}
-              <textarea className="form-control form-group min-h-[65px]" placeholder="Mensaje" id="mensaje" onChange={handleChange}></textarea>
-              {errors.mensaje && <small className="text-[#c53568]">{errors.mensaje}</small>}
-              <button className="form_submit" onClick={handleSubmit}>Enviar</button>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  className="form-control form-group"
+                  placeholder="Nombre"
+                  id="nombre"
+                  onChange={handleChange}
+                />
+                {errors.nombre && (
+                  <small className="text-[#c53568]">{errors.nombre}</small>
+                )}
+                <input
+                  type="email"
+                  className="form-control form-group"
+                  placeholder="Email"
+                  id="email"
+                  onChange={handleChange}
+                />
+                {errors.email && (
+                  <small className="text-[#c53568]">{errors.email}</small>
+                )}
+                <textarea
+                  className="form-control form-group min-h-[65px]"
+                  placeholder="Mensaje"
+                  id="mensaje"
+                  onChange={handleChange}
+                ></textarea>
+                {errors.mensaje && (
+                  <small className="text-[#c53568]">{errors.mensaje}</small>
+                )}
+                <button type="submit" className="form_submit">
+                  Enviar
+                </button>
+              </form>
             </div>
           </div>
         </div>
