c3cd1e3d31d19d2c424fa8d14e1fd7d41f6f2e5e
guirep <guirep@gmail.com>
2024-04-06 12:24:55 +0200

    Contaco : he añadido los campos y le he dado estilo

--- src/Componentes/Contact/Contact.jsx
+++ src/Componentes/Contact/Contact.jsx
@@ -13,9 +13,33 @@ const Contact = () => {
         </div>
 
         <div className="w-full mt-24 h-[600px] flex justify-center items-center ">
-
-          <div className='formulario_padre w-[100%] md:w-[60%] h-[90%] border-2 border-[#161455] bg-[#161455] flex flex-col-reverse justify-start'>
-            <div className="w-[100%] h-[100px] md:w-[150px] md:h-[100%] bg-[#7e57c2]"></div>
+          <div className="formulario_padre w-[100%] md:w-[60%] h-[90%] border-2 border-[#161455] bg-[#161455] flex flex-col-reverse md:flex-row justify-start">
+            <div className="div_lateral w-[100%] h-[100px] md:w-[20%] md:h-[100%] bg-[#7e57c2]"></div>
+            <div className="contacto_campos md:w-[80%] py-12 px-24">
+              <h3 className="text-white font-bold">Contacta-me</h3>
+              <p>
+                Siéntete libre de contactarnos en cualquier momento. Nos
+                pondremos en contacto contigo tan pronto como sea posible!
+              </p>
+              <input
+                type="text"
+                className="form-control form-group"
+                placeholder="Nombre"
+                id="nombre"
+              />
+              <input
+                type="text"
+                className="form-control form-group"
+                placeholder="Email"
+                id="email"
+              />
+              <textarea
+                className="form-control form-group min-h-[65px]"
+                placeholder="Mensaje"
+                id="mensaje"
+              ></textarea>
+              <button className="form_submit">Enviar</button>
+            </div>
           </div>
         </div>
       </div>
