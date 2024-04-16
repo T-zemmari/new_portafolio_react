4b78eb8f539207d98a19ef9f9b82e9e43114f828
Tarek <tarek@tedybusiness.com>
2024-04-16 07:54:57 +0200

    SKills , solo estoy usando la opacity para animar

--- src/Componentes/Skills/Skills.jsx
+++ src/Componentes/Skills/Skills.jsx
@@ -108,8 +108,8 @@ const Skills = () => {
       <div className="w-full min-h-[500px] flex flex-col lg:flex-row gap-2">
         <motion.div
           ref={cardsRef}
-          initial={{ y: "-50%", opacity: 0 }}
-          animate={showCards.includes(cardsRef.current) ? { y: 0, opacity: 1 } : {}}
+          initial={{opacity: 0 }}
+          animate={showCards.includes(cardsRef.current) ? { opacity: 1 } : {}}
           transition={{ duration: 0.3 }}
           className="flex w-full lg:w-3/6 h-[400px] mt-10 rounded-[10px] flex-col md:flex-row justify-center items-center gap-2 md:flex-wrap p-4 lg:mt-32"
           id="contenedor_tarjetas_skills"
@@ -126,8 +126,8 @@ const Skills = () => {
         </motion.div>
         <motion.div
           ref={skillsRef}
-          initial={{ y: "50%", opacity: 0 }}
-          animate={showCards.includes(skillsRef.current) ? { y: 0, opacity: 1 } : {}}
+          initial={{  opacity: 0 }}
+          animate={showCards.includes(skillsRef.current) ? { opacity: 1 } : {}}
           transition={{ duration: 0.3 }}
           className="w-full lg:w-3/6 h-[400px] flex flex-col justify-center items-start  gap-4 rounded-[10px] mt-10 border border-[#403ca3] md:p-4 lg:mt-32"
           id="contenedor_info_skills"
